821a4c89956ee79d74b2f4c816acb7b7f6489556
zenlex <erichrkeil@gmail.com>
2022-05-17 18:57:46 -0500

    add request logging and env config

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import mountRoutes from './controllers/index.js';
+import { requestLogger } from './middleware.js';
 
 const app = express();
 app.use(express.json());
+app.use(requestLogger);
+
 mountRoutes(app);
 
 export default app;
--- src/db/index.js
+++ src/db/index.js
@@ -1,20 +1,24 @@
 import pg from 'pg';
+import dotenv from 'dotenv';
+import process from 'node:process';
+dotenv.config();
 
 const { Pool } = pg;
 
-const connectionString = 'postgres://xwvheyws:6oL9FOrBkJKw5zEckx8cbBku8IgLY35M@heffalump.db.elephantsql.com/xwvheyws';
+const connectionString = process.env.PG_CONNECTION_URL;
 
 const pool = new Pool({connectionString});
-(async () =>  {
-	const client = await pool.connect();
-	console.log('testing pool query');
-	try{
-		const res = await client.query('SELECT * FROM tester;');
-		console.log(res.rows);
-	} finally {
-		client.release();
-	}
-})().catch(err => console.log(err.stack));
+// INITIALIZATION TEST
+// (async () =>  {
+// 	const client = await pool.connect();
+// 	console.log('testing pool query');
+// 	try{
+// 		const res = await client.query('SELECT * FROM tester;');
+// 		console.log(res.rows);
+// 	} finally {
+// 		client.release();
+// 	}
+// })().catch(err => console.log(err.stack));
 
 const query = (text, params) => pool.query(text, params);
 
